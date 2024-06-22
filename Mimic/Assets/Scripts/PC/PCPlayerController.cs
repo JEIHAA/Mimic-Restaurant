0c9fabc515eed8ea4189c87178eea98bc7f5772f
HuiEon Lee <61311298+JEIHAA@users.noreply.github.com>
2024-06-22 17:34:09 +0900

    Resturant

---
+++ Mimic/Assets/Scripts/PC/PCPlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCPlayerController : MonoBehaviour
+{
+    private void PCPlayerMove()
+    {
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            //this.transform.position
+        }
+    }
+}
