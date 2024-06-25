5cdc5db0420a37940294716289230305647b7e69
KoYoungSuk <vheh5678@naver.com>
2024-06-25 09:17:17 +0900

    COMMIT: JUNE 25 2024

--- Mimic/Assets/Scripts/VR/HandCtrl.cs
+++ Mimic/Assets/Scripts/VR/HandCtrl.cs
@@ -50,7 +50,7 @@ public class HandCtrl : MonoBehaviour
         {
             if (gun.activeSelf && !triggerPressed)
             {
-                gunComponent.Shoot();
+                gunComponent.ShootBullet();
                 triggerPressed = true ; 
             }
         }
