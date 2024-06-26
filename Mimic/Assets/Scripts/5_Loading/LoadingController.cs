72f4a26d54bb5a6082ca32e8ae1adf39abe0490b
KoYoungSuk <vheh5678@naver.com>
2024-06-26 11:33:28 +0900

    COMMIT: 2024-06-26

--- Mimic/Assets/Scripts/5_Loading/LoadingController.cs
+++ Mimic/Assets/Scripts/5_Loading/LoadingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
 
 //2024-05-22: CUSTOM UNITY TEMPLATE 
 
@@ -11,38 +12,70 @@ public class LoadingController : MonoBehaviour
 
     [SerializeField] private Image loadingimage = null;
     [SerializeField] private TextMeshProUGUI loadingtext = null;
+    [SerializeField, Range(0f, 0.1f)] private float WaitSecond_Image = 0.08f; 
+    [SerializeField, Range(0f, 1f)] private float WaitSecond_Text = 0.25f;
+    [SerializeField] private Canvas canvas = null;
+    [SerializeField] private GameObject vr = null;
+    [SerializeField] private Camera pccamera = null;
 
     private bool isLoading = true;
 
-    private void Start()
+    private AsyncOperation op = null;
+
+    private void Awake()
     {
-        StartCoroutine(LoadingCoroutine());
+        
     }
 
-    private IEnumerator LoadingCoroutine()
+    private void Start()
+    {
+        if(XRSettings.enabled)
+        {
+            //VRÀÏ¶§ 
+        }
+        else
+        {
+
+        }
+        StartCoroutine(LoadingSprite());
+        StartCoroutine(LoadingText()); 
+    }
+
+
+    private IEnumerator LoadingSprite()
     {
         int i = 0;
-        float t = 0f; 
-        loadingtext.text = "Loading";
-        
+
         while (isLoading)
         {
             if (i > 95)
             {
                 i = 0;
             }
-            loadingtext.text += "."; 
-            loadingimage.sprite = Resources.Load<Sprite>("Images\\Sprites\\Loading_Sprites\\" + i + "");
+            loadingimage.sprite = Resources.Load<Sprite>("Images\\Sprites\\Loading_Sprites\\" + i);
             ++i;
+            yield return new WaitForSeconds(WaitSecond_Image); 
+        }
+        yield break;
+    }
+
+    private IEnumerator LoadingText()
+    {
+        loadingtext.text = "Loading";
+        float t = 0f;
+        while (isLoading)
+        {
+            loadingtext.text += ".";
             ++t;
-            yield return new WaitForSeconds(0.5f); 
-            if(t >= 3f)
+            yield return new WaitForSeconds(WaitSecond_Text);
+            if (t >= 3f)
             {
                 loadingtext.text = "Loading";
-                t = 0f; 
+                t = 0f;
             }
         }
         yield break;
     }
+
 
 }
