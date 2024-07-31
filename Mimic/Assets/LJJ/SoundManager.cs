75d82f8985fe37f26c47db03097a87e17072e601
1Pops1 <ggdodo0000@gmail.com>
2024-07-31 17:17:59 +0900

    final

---
+++ Mimic/Assets/LJJ/SoundManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class SoundManager : MonoBehaviour
+{
+    public string name;
+    public AudioClip clip;
+
+}
